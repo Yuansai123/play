959ce10f3cfbb029d66298e0765871fd67321e27
Mac <475013647@qq.com>
2020-05-24 08:46:16 +0800

    up

---
+++ Desktop/html基础/reacttsx/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home from './pages/home'
+function App() {
+  return (
+    <div className="App">
+      <div>12312312323(app.js)</div>
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
